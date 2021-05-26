1649957bf80a83a74a9c94c159620c251fb6543d
Neelesh Mishra <neeleshojmishra@gmail.com>
2021-05-26 18:43:03 +0530

    Adding saving features

--- src/App.vue
+++ src/App.vue
@@ -31,19 +31,34 @@
       };
     },
 
+    created() {
+      const todos = localStorage.getItem("todos");
+      if (todos) {
+        this.todos = JSON.parse(todos);
+      }
+    },
+
     methods: {
+      updateStorage() {
+        localStorage.setItem("todos", JSON.stringify(this.todos));
+      },
+
       newTodo(text) {
         this.todos.push({ id: this.todos.length, text: text, done: false });
+        this.updateStorage();
       },
 
       toggleTodo(id) {
         this.todos = this.todos.map((obj) =>
           obj.id == id ? { ...obj, done: !obj.done } : obj
         );
+
+        this.updateStorage();
       },
 
       removeTodo(id) {
         this.todos = this.todos.filter((obj) => obj.id != id);
+        this.updateStorage();
       },
     },
   };
